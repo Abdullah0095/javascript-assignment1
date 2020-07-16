9eeb70a2e2346a7f4fe5b18a37c635c785c05594
unknown <abdullah.web1995@gmail.com>
2020-07-16 23:47:48 +0600

    number1 is done

--- assignment.js
+++ assignment.js
@@ -1,1 +1,19 @@
-// javascript
+// Assignment 1 :: Conversion of FEET to MILE
+ 
+function feetToMile(feet){
+    if(feet < 0){
+        console.log("WARNING: Distance cannot be negative");
+    }else{
+        var mile = feet/5280;
+        return mile;
+    }
+}
+
+         
+         
+         var calculation = feetToMile();
+         console.log(calculation);
+
+
+// Assignment 2 ::          
+                                      
